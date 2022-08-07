fb2743e4e1d0f965525a4e97b7d42e2a6fdc2dd3
Kelly Duran <kaduran16@gmail.com>
2022-08-07 13:20:40 -0400

    Done with this assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,20 @@
-# Your code here!
+def greet_programmer
+    puts 'Hello, programmer!'
+end 
+
+def greet name
+    puts "Hello, #{ name }!"
+end
+
+def greet_with_default( name = "programmer" )
+    puts "Hello, #{ name }!"
+end
+
+def add( num1, num2 )
+    num1 + num2
+end
+
+def halve( num1 )
+    return nil if num1.class != Integer
+    return num1/2
+end
